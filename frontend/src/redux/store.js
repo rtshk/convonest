3c90ca501233dbb474868d0b74ad88dcb3402ac6
rtshk <riteshnzee@gmail.com>
2024-11-27 19:48:14 +0530

    added redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,14 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import messageReducer from "./messageSlice";
 import socketReducer from "./socketSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const rootReducer = combineReducers({
+  user: userReducer,
+  messages: messageReducer,
+  socket: socketReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    messages: messageReducer,
-    socket: socketReducer,
-  },
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
