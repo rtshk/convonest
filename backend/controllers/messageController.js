274d87829a9f74585d9ad0ca4b7f3d0993818fc8
rtshk <riteshnzee@gmail.com>
2024-11-27 19:43:20 +0530

    showing only those users whom we have had conversations with and exculding the logged in user from showing up

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -6,10 +6,16 @@ const { io, userIdToSocketId } = require("../socket/socket");
 exports.sendChatItems = async (req, res) => {
   try {
     const userId = req.id;
-    const chatItems = await User.find({ _id: { $ne: userId } }).select(
-      "-password"
-    );
-    res.status(200).json(chatItems);
+    const chatItems = await Conversation.find({
+      participants: userId,
+    }).populate("participants");
+    const otherUsers = chatItems.map((conversation) => {
+      const otherUser = conversation.participants.filter((otherUserId) => {
+        return userId != otherUserId._id;
+      });
+      return otherUser[0];
+    });
+    res.status(200).json(otherUsers);
   } catch (error) {
     console.log(error);
   }
@@ -17,8 +23,6 @@ exports.sendChatItems = async (req, res) => {
 
 exports.sendMessage = async (req, res) => {
   try {
-    
-
     const { message } = req.body;
     const senderId = req.id;
     const receiverId = req.params.id;
@@ -42,11 +46,13 @@ exports.sendMessage = async (req, res) => {
     if (newMessage) {
       gotConversation.messages.push(newMessage._id);
     }
+    const sender = await User.findOne({_id : senderId})
+
     await Promise.all([gotConversation.save(), newMessage.save()]);
     const receiverSocketId = userIdToSocketId(receiverId);
     // console.log("socket",receiverSocketId);
-    if(receiverSocketId){
-      io.to(receiverSocketId).emit("newMessage", newMessage);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage, sender);
     }
     return res.status(201).json({ newMessage });
   } catch (error) {
@@ -65,6 +71,23 @@ exports.receiveMessage = async (req, res) => {
 
     return res.status(200).json(getConversation);
   } catch (error) {
-    console.log("error while running receive message control",error);
+    console.log("error while running receive message control", error);
+  }
+};
+exports.searchUser = async (req, res) => {
+  try {
+    const userId = req.id;
+    const { searchedUser } = req.query;
+    if(!searchedUser) return res.status(200).json([]);
+
+    const user = await User.find({ username: {$regex : `^${searchedUser}`} });
+    if (user.length == 0) {
+      return res.status(404).json({ message: "no user found" });
+    }
+    const filteredUser = user.filter((user)=>{ return user._id != userId}); 
+
+    return res.status(200).json(filteredUser);
+  } catch (error) {
+    console.log(error)
   }
 };
