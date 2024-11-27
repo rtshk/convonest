2477fedb2c704dac7b8ffecd9d92d80ca2d3d30b
rtshk <riteshnzee@gmail.com>
2024-11-27 19:43:51 +0530

    add a new route to allow to search any user on the app

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const {sendChatItems, sendMessage, receiveMessage} = require("../controllers/messageController")
+const {sendChatItems, sendMessage, receiveMessage, searchUser} = require("../controllers/messageController")
 const router = express.Router();
 
 router.route("/").get(authMiddleware,sendChatItems);
 router.route("/sendmessage/:id").post(authMiddleware,sendMessage);
 router.route("/receivemessage/:id").get(authMiddleware,receiveMessage);
+router.route("/search").get(authMiddleware, searchUser)
 
 module.exports = router;
