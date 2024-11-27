741265140b84a606ece95eb75b4fe8f5803a123c
rtshk <riteshnzee@gmail.com>
2024-11-27 19:47:33 +0530

    notification sound when new message is received

--- frontend/src/hooks/useSocketMessage.js
+++ frontend/src/hooks/useSocketMessage.js
@@ -1,19 +1,27 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setMessages } from '../redux/messageSlice';
+import {Howl} from "howler";
+import { toast } from 'react-toastify';
+
 
 const useSocketMessage = () => {
     const {messages} = useSelector(store=>store.messages);
     const {currentUser} = useSelector(store=>store.user)
     const {socket} = useSelector(store => store.socket);
     const dispatch = useDispatch();
+    const notificationSound = new Howl({src : ['/notification.wav'],});
     useEffect(()=>{
-        socket.on('newMessage', (newMessage)=>{
-            if(newMessage?.senderId === currentUser?._id){
-                console.log("hook runs")
-                dispatch(setMessages([...messages,newMessage]));
-            }
-        })
+        if(socket){
+            socket.on('newMessage', (newMessage, sender)=>{            
+                if(newMessage?.senderId === currentUser?._id){
+                    dispatch(setMessages([...messages,newMessage]));
+                }else{
+                    notificationSound.play();
+                    toast.info(`${sender.username} sent a new message`)
+                }
+            })
+        }
         return ()=>{
             socket?.off("newMessage");
         }
