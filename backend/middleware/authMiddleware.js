fdb6c2828f51ae8826ae658a7529e009a53def48
rtshk <riteshnzee@gmail.com>
2024-11-28 22:49:16 +0530

    rollback

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,9 +4,7 @@ const authMiddleware = async (req, res, next) => {
 
    try {
     const token = req.cookies.token;
-    console.log("given token")
-    console.log(token)
-    console.log("token end")
+
 
     if(!token){
        return res.status(401).json({message : "token not provided"});
